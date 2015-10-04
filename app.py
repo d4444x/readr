b1b895fd7e716e7a3774955b7e3440db3d56344e
tmulc18 <tmulc18@gmail.com>
2015-10-04 00:49:51 -0400

    more json switched

--- app.py
+++ app.py
@@ -8,14 +8,14 @@ app.debug = True
 @app.route("/get_time", methods=['POST'])
 def get_info():
     try:
-        user_id = request.form['user_id']
-        url = request.form['url']
-	index = request.form['index']
+        user_id = request.json[0]['user_id']
+        url = request.json[0]['url']
+	index = request.json[0]['index']
     except Exception, e:
         return str(e)
     content = getwords.get_words(url)
     t = timetotommy.calculate_time(content, user_id)
-    return {"time": str(t),"index": index}
+    return jsonify({"time": str(t),"index": index})
 
 @app.route("/record_time", methods=['POST'])
 def save_info():
